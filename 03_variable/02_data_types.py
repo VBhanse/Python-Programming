572a1766a4ae5f2fbbf45d5566e7b3a0accb88f4
Vivek <vivek26041994@gmail.com>
2025-09-11 00:41:34 +0530

    basics of python

---
+++ 03_variable/02_data_types.py
@@ -0,0 +1,17 @@
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.2
+print(cgpa)
+print(type(cgpa))
+
+name = "Harry"
+print(name)
+print(type(name))
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
+
+print("shortcut successful")
